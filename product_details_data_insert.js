eb592bb7de8dc67522b8f2d9ffbbef87ed0d36dd
TanjimAyan <88281071+TanjimAyan@users.noreply.github.com>
2021-10-14 21:20:46 +0600

    Delete product_details_data_insert.js

--- product_details_data_insert.js
+++
@@ -1,9 +0,0 @@
-var url_string = window.location.href;
-var url = new URL(url_string);
-
-var product_img = url.searchParams.get("product_img");
-var cate = url.searchParams.get("cate");
-
-document.querySelector('.book_img_wrapper img').setAttribute("src", "images/" + product_img);
-
-document.querySelector('.product_details_sec1 > .col:first-child .details_wrapper .cate a').innerHTML = cate;
